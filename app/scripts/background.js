0eb4a0838783344997d193f8c63d6754718406c5
TravisTX <travistx@gmail.com>
2015-06-20 09:55:28 -0400

    style(app): code formatting

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -4,7 +4,7 @@ chrome.runtime.onInstalled.addListener(function (details) {
     console.log('previousVersion', details.previousVersion);
 });
 
-chrome.runtime.onMessage.addListener(function(msg, sender) {
+chrome.runtime.onMessage.addListener(function (msg, sender) {
     if ((msg.from === 'content') && (msg.subject === 'showPageAction')) {
         // enable the page-action for the requesting tab
         chrome.pageAction.show(sender.tab.id);
--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -5,15 +5,15 @@ Init();
 function Init() {
     ShowPageActionIcon();
 
-    $(document).ready(function() {
+    $(document).ready(function () {
         ApplyEnhancements();
     });
 }
 
 function ApplyEnhancements() {
-	AddNewLink();
-	AddRowStyles();
-	AddWiStyles();
+    AddNewLink();
+    AddRowStyles();
+    AddWiStyles();
 }
 
 function ShowPageActionIcon() {
@@ -25,108 +25,108 @@ function ShowPageActionIcon() {
 }
 
 function AddNewLink() {
-	if (UrlContains('/_backlogs') || UrlContains('/_workitems')) {
-		$('.hubs-section ul').append('<li><a href="/tfs/DefaultCollection/CareBookScrum/_workItems/create/Product%20Backlog%20Item">+ New PBI</a></li>');
-		$('.hubs-section ul').append('<li><a href="/tfs/DefaultCollection/CareBookScrum/_workItems/create/Bug">+ New Bug</a></li>');
-	}
+    if (UrlContains('/_backlogs') || UrlContains('/_workitems')) {
+        $('.hubs-section ul').append('<li><a href="/tfs/DefaultCollection/CareBookScrum/_workItems/create/Product%20Backlog%20Item">+ New PBI</a></li>');
+        $('.hubs-section ul').append('<li><a href="/tfs/DefaultCollection/CareBookScrum/_workItems/create/Bug">+ New Bug</a></li>');
+    }
 }
 
 function AddRowStyles() {
-	if (UrlContains('taskboard')) {
-		console.log('doin row styles');
-		var tfsPayloadData = JSON.parse($('#taskboard script')[0].text).payload.data;
-		
-		var $rows = $('td.taskboard-parent.highlight-on-row-change');
-		$rows.each(function() {
-			var $row = $(this);
-			var rowId = $row.attr('id').substr(17); // todo: smarter way to find the id here.
-			//$row.find(".witTitle").text("test");
-			var rowType = tfsPayloadData[rowId][1]; // todo: smarter way to do the magic index here.
-			
-			$row.siblings('.taskboard-expander').css({
-				'background-color': RowTypeToColor(rowType),
-				'border-top': '5px solid white',
-				'border-bottom': '5px solid white'
-			});
+    if (UrlContains('taskboard')) {
+        console.log('doin row styles');
+        var tfsPayloadData = JSON.parse($('#taskboard script')[0].text).payload.data;
 
-			$row.css({
-				'padding-left': '5px'
-			});
-			$row.attr('title', rowType);
-		});
-	}
+        var $rows = $('td.taskboard-parent.highlight-on-row-change');
+        $rows.each(function () {
+            var $row = $(this);
+            var rowId = $row.attr('id').substr(17); // todo: smarter way to find the id here.
+            //$row.find(".witTitle").text("test");
+            var rowType = tfsPayloadData[rowId][1]; // todo: smarter way to do the magic index here.
+
+            $row.siblings('.taskboard-expander').css({
+                'background-color': RowTypeToColor(rowType),
+                'border-top': '5px solid white',
+                'border-bottom': '5px solid white'
+            });
+
+            $row.css({
+                'padding-left': '5px'
+            });
+            $row.attr('title', rowType);
+        });
+    }
 }
 
 function AddWiStyles() {
-	if (UrlContains('taskboard')) {
-		console.log('doin wi styles');
-		var tfsPayloadData = JSON.parse($('#taskboard script')[0].text).payload.data;
-		
-		var $cards = $('.tbTile');
-		$cards.each(function() {
-			var $card = $(this);
-			var cardId = $card.attr('id').substr(5); // todo: smarter way to find the id here.
-			var activity = tfsPayloadData[cardId][7]; // todo: smarter way to do the magic index here.
-			
-			var colors = ActivityToColor(activity);
-			$card.find('.tbTileContent').css({
-				'background-color': colors.light,
-				'border-left-color': colors.dark
-			});
-			$card.attr('title', activity);
-		});
-	}
+    if (UrlContains('taskboard')) {
+        console.log('doin wi styles');
+        var tfsPayloadData = JSON.parse($('#taskboard script')[0].text).payload.data;
+
+        var $cards = $('.tbTile');
+        $cards.each(function () {
+            var $card = $(this);
+            var cardId = $card.attr('id').substr(5); // todo: smarter way to find the id here.
+            var activity = tfsPayloadData[cardId][7]; // todo: smarter way to do the magic index here.
+
+            var colors = ActivityToColor(activity);
+            $card.find('.tbTileContent').css({
+                'background-color': colors.light,
+                'border-left-color': colors.dark
+            });
+            $card.attr('title', activity);
+        });
+    }
 }
 
 function UrlContains(match) {
-	return document.URL.toLowerCase().indexOf(match.toLowerCase()) >= 0;
+    return document.URL.toLowerCase().indexOf(match.toLowerCase()) >= 0;
 }
 
 function RowTypeToColor(rowType) {
-	if (rowType === 'Bug') {
-		return '#CC293D';
-	}
-	return '#009CCC';
+    if (rowType === 'Bug') {
+        return '#CC293D';
+    }
+    return '#009CCC';
 }
 
 function ActivityToColor(activity) {
-	var knownActivity = false;
-	var activityHue = 207;
-	if (activity === 'Deployment') {
-		knownActivity = true;
-		activityHue = 300;
-	}
-	if (activity === 'Design') {
-		knownActivity = true;
-		activityHue = 60;
-	}
-	if (activity === 'Development') {
-		knownActivity = true;
-		activityHue = 180;
-	}
-	if (activity === 'Documentation') {
-		knownActivity = true;
-		activityHue = 120;
-	}
-	if (activity === 'Requirements') {
-		knownActivity = true;
-		activityHue = 240;
-	}
-	if (activity === 'Testing') {
-		knownActivity = true;
-		activityHue = 0;
-	}
+    var knownActivity = false;
+    var activityHue = 207;
+    if (activity === 'Deployment') {
+        knownActivity = true;
+        activityHue = 300;
+    }
+    if (activity === 'Design') {
+        knownActivity = true;
+        activityHue = 60;
+    }
+    if (activity === 'Development') {
+        knownActivity = true;
+        activityHue = 180;
+    }
+    if (activity === 'Documentation') {
+        knownActivity = true;
+        activityHue = 120;
+    }
+    if (activity === 'Requirements') {
+        knownActivity = true;
+        activityHue = 240;
+    }
+    if (activity === 'Testing') {
+        knownActivity = true;
+        activityHue = 0;
+    }
 
-	var result = {
-		light: 'hsl(0, 0%, 90%)',
-		dark: 'hsl(0, 0%, 69%)',
-	};
+    var result = {
+        light: 'hsl(0, 0%, 90%)',
+        dark: 'hsl(0, 0%, 69%)',
+    };
 
-	if (knownActivity) {
-		result = {
-			light: 'hsl(' + activityHue + ', 50%, 85%)',
-			dark: 'hsl(' + activityHue + ', 50%, 69%)'
-		}; 
-	}
-	return result;
+    if (knownActivity) {
+        result = {
+            light: 'hsl(' + activityHue + ', 50%, 85%)',
+            dark: 'hsl(' + activityHue + ', 50%, 69%)'
+        };
+    }
+    return result;
 }
